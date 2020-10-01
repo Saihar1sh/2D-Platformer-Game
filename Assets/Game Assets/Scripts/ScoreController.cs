73df368f24a5915790dc4134e9d413cfae58d80a
Har15h <saiharishmanne@gmail.com>
2020-10-01 21:53:53 +0530

    made a collectible key and made the score increase when key is collected

---
+++ Assets/Game Assets/Scripts/ScoreController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class ScoreController : MonoBehaviour
+{
+    private int score;
+    private TextMeshProUGUI scoreText;
+    private void Awake()
+    {
+        scoreText = GetComponent<TextMeshProUGUI>();
+    }
+    private void Start()
+    {
+        RefreshUI();
+    }
+    public void IncreaseScore(int increment)
+    {
+        score += increment;
+        RefreshUI();
+    }
+
+    private void RefreshUI()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+}
