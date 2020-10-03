51ae30614b434d70b2d38dd4b7804b48f965e39b
Har15h <saiharishmanne@gmail.com>
2020-10-03 21:41:11 +0530

    Added a idle enemy, made player die when player collides with enemy and the scene is reloaded.

---
+++ Assets/Game Assets/Scripts/EnemyController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerController>() != null)
+        {
+            PlayerController pc = collision.gameObject.GetComponent<PlayerController>();
+            pc.KillPlayer();
+        }
+    }
+}
