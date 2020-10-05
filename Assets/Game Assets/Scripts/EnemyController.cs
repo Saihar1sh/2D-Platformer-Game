92da9a2a8a684066ad3a4a90210c77c26f11a613
Har15h <saiharishmanne@gmail.com>
2020-10-06 00:39:05 +0530

    made enemy flip when not on ground and made an idiot enemy which falls down the platform and it can be made that way with boolean

--- Assets/Game Assets/Scripts/EnemyController.cs
+++ Assets/Game Assets/Scripts/EnemyController.cs
@@ -1,7 +1,58 @@
-﻿using UnityEngine;
+﻿using System.ComponentModel;
+using System.Runtime.InteropServices.ComTypes;
+using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    public bool idiotEnemy = false;
+    
+    private bool isGrounded;
+    private int facingDirection = 1;
+    [SerializeField]
+    private float enemySpeed, groundCheckDist;
+    [SerializeField]
+    private LayerMask whatIsGround;
+    [SerializeField]
+    private Transform groundCheck;
+
+
+    private void Update()
+    {
+        Vector3 scale = transform.localScale;
+        scale.x = facingDirection * Mathf.Abs(scale.x);
+        transform.localScale = scale;
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckDist, whatIsGround);
+
+    }
+    private void FixedUpdate()
+    {
+        if (!idiotEnemy)
+        {
+            if (!isGrounded)
+            {
+                facingDirection *= -1;
+            }
+            else
+            {
+                Vector3 position = transform.position;
+                position.x += facingDirection * enemySpeed * Time.deltaTime;
+                transform.position = position;
+
+            }
+        }
+        else
+        {
+            Vector3 position = transform.position;
+            position.x += facingDirection * enemySpeed * Time.deltaTime;
+            transform.position = position;
+
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(groundCheck.position, new Vector2(groundCheck.position.x, groundCheck.position.y - groundCheckDist));
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.GetComponent<PlayerController>() != null)
