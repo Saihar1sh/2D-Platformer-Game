bc6211edabadd045c347c771acfde4a568061460
Har15h <saiharishmanne@gmail.com>
2020-10-09 18:49:07 +0530

    made an option for player to select a Level to play

---
+++ Assets/Game Assets/Scripts/Level/LevelLoader.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Button))]
+public class LevelLoader : MonoBehaviour
+{
+    private Button button;
+    [SerializeField]
+    private string LevelName;
+
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(click);
+    }
+    private void click()
+    {
+        SceneManager.LoadScene(LevelName);
+    }
+
+}
